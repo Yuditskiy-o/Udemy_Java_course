74ac742ef4781c2941052b0ac257609640b1f03f
Oleg Yuditskiy <komekadze@gmail.com>
2021-08-28 15:10:01 +0700

    Homework

--- src/main/java/Lesson_1/Main.java
+++ src/main/java/Lesson_1/Main.java
@@ -3,16 +3,12 @@ package Lesson_1;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.print("A ");
-        System.out.println("a");
-        System.out.print("B ");
-        System.out.println("b");
-        System.out.print("C ");
-        System.out.println("c");
-
-        System.out.println("A B C D E F");
-        System.out.println();
-        System.out.println("G H I G K L");
+        System.out.println("                Рубаи");
+        System.out.println("Много лет размышлял я над жизнью земной.");
+        System.out.println("Непонятного нет для меня под луной.");
+        System.out.println("Мне известно, что мне ничего не известно!");
+        System.out.println("Вот последняя правда открытая мной.");
+        System.out.println("                                О. Хайям");
 
     }
 }
