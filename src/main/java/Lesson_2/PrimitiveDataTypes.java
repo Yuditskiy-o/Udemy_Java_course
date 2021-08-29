29824a7a7c7660673f0cc8cd3db965f0e34df08e
Oleg Yuditskiy <komekadze@gmail.com>
2021-08-29 22:23:49 +0700

    Homework

---
+++ src/main/java/Lesson_2/PrimitiveDataTypes.java
@@ -0,0 +1,71 @@
+package Lesson_2;
+
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        byte b1 = 12;
+        byte b2 = 0B1100;
+        byte b3 = 014;
+        byte b4 = 0XC;
+
+        short sh1 = 1300;
+        short sh2 = 0B10100010100;
+        short sh3 = 02424;
+        short sh4 = 0X514;
+
+        int i1 = 0;
+        int i2 = 0B0;
+        int i3 = 00;
+        int i4 = 0X0;
+
+        long l1 = 123456789;
+        long l2 = 0B111010110111100110100010101;
+        long l3 = 0726746425;
+        long l4 = 0X75BCD15;
+
+        float f1 = 3.1f;
+        float f2 = 10.1f;
+
+        float d1 = 6.1f;
+        float d2 = 40.1f;
+
+        boolean bool1 = true;
+        boolean bool2 = false;
+
+        char c1 = '?';
+        char c2 = 17476;
+        char c3 = '\u1334';
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+        System.out.println(b4);
+
+        System.out.println(sh1);
+        System.out.println(sh2);
+        System.out.println(sh3);
+        System.out.println(sh4);
+
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(i3);
+        System.out.println(i4);
+
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(l3);
+        System.out.println(l4);
+
+        System.out.println(f1);
+        System.out.println(f2);
+
+        System.out.println(d1);
+        System.out.println(d2);
+
+        System.out.println(true);
+        System.out.println(false);
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+    }
+}
