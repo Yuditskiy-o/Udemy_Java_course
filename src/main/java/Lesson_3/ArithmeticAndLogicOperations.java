669e0ec307d97446b43ba89347d2441f18cffb64
Oleg Yuditskiy <komekadze@gmail.com>
2021-09-05 22:31:15 +0700

    Homework, lesson 3

---
+++ src/main/java/Lesson_3/ArithmeticAndLogicOperations.java
@@ -0,0 +1,24 @@
+package Lesson_3;
+
+public class ArithmeticAndLogicOperations {
+    public static void main(String[] args) {
+        int i1 = 5;
+        int i2 = 11;
+        double d1 = 5.5;
+        double d2 = 1.3;
+        long l = 20L;
+        double result = i2 / d1 + d2 % i1 - l;
+
+        System.out.println(result);
+
+        int a = 5;
+        int a1 = a-- - --a + ++a + a++ + a;
+
+        System.out.println(a1);
+
+        int b = 8;
+        int b1 = ++b - b++ + ++b - --b;
+
+        System.out.println(b1);
+    }
+}
