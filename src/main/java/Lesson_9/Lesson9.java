d75e1499d4dccf1f8ddb44955c3f0732d941c6fc
Oleg Yuditskiy <komekadze@gmail.com>
2021-11-05 13:51:21 +0700

    Homework lesson 9

---
+++ src/main/java/Lesson_9/Lesson9.java
@@ -0,0 +1,17 @@
+package Lesson_9;
+
+public class Lesson9 {
+
+    public static void abc() {
+        String s1 = new String("ABC");
+        String s2 = new String("DEF");
+    }
+
+    public static void main(String[] args) {
+        Lesson9 lesson9 = new Lesson9();
+        abc();
+        abc();
+        String s1 = new String("ABC");
+        abc();
+    }
+}
